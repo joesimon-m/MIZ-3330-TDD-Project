336100abdbaacbf98f5f9eab75793ceaea1ec79f
joesimon-m <jasdfw@umsystem.edu>
2025-04-22 01:50:05 -0500

    Instantiated some of the methods inside of the Game class

--- src/hw4/game/Game.java
+++ src/hw4/game/Game.java
@@ -1,36 +1,45 @@
 package hw4.game;
 
-import hw4.maze.Grid;
-import hw4.player.Movement;
-import hw4.player.Player;
+	import hw4.maze.Grid;
+	import hw4.player.Movement;
+	import hw4.player.Player;
 
-public class Game {
+	public class Game {
 
-	private Grid grid;
+		private Grid grid;
 
-	public Game(int i) {
-		
+		public Game(int i) {
+			if (i < 3 || i > 7) {
+				this.grid = null;
+			} else {
+				this.grid = createRandomGrid(i); // Creates a random grid of ixi size
+			}
+		}
+
+		public Game(Grid grid) {
+			this.grid = grid;
+		}
+
+		public Grid getGrid() {
+			// TODO Auto-generated method stub
+			return this.grid;
+		}
+
+		public boolean play(Movement movement, Player player) {
+			if (movement == null || player == null) {
+				return false;
+			} else {
+				return false; // *****still has to be implemented*****
+			}
+		}
+
+		public void setGrid(Grid grid) {
+			this.grid = grid;
+		}
+
+		public Grid createRandomGrid(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
 	}
-
-	public Game(Grid grid) {
-		
-	}
-
-	public Grid getGrid() {
-		return null;
-	}
-
-	public boolean play(Movement movement, Player player) {
-		return false;
-	}
-
-	public void setGrid(Grid grid) {
-		
-	}
-
-	public Grid createRandomGrid(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
