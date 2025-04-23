badc2d775d5964fb3536b004f783fae02c170cc3
vdawson17 <vincegdawson@gmail.com>
2025-04-22 22:27:00 -0500

    added method to display grid during simulation

--- src/hw4/Main.java
+++ src/hw4/Main.java
@@ -27,13 +27,38 @@ public class Main {
             agentRow = getRandomPosition(gridSize);
             agentCol = getRandomPosition(gridSize);
         }
-  
+
+        Player player = new Player(
+                game.getGrid().getRows().get(agentRow),
+                game.getGrid().getRows().get(agentRow).getCells().get(agentCol)
+        );
+
+
+        displayGrid(player);}
+
+
+    private static void displayGrid(Player player) {
+        System.out.println("Current Grid State:");
+        ArrayList<Row> rows = game.getGrid().getRows();
+        for (int i = 0; i < rows.size(); i++) {
+            for (int j = 0; j < rows.get(i).getCells().size(); j++) {
+                if (i == 0 && j == 0) {
+                    System.out.print("E ");
+                } else if (i == rows.indexOf(player.getCurrentRow()) &&
+                           j == rows.get(i).getCells().indexOf(player.getCurrentCell())) {
+                    System.out.print("A ");
+                } else {
+                    System.out.print("S ");
+                }
+            }
+            System.out.println();
         }
+        System.out.println();
+    }
 
     private static int getRandomPosition(int gridSize) {
         Random rand = new Random();
         return rand.nextInt(gridSize);
     }
 
-  
 }
